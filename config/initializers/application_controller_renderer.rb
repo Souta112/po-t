d212d2ef1dc625f78592f5fe1394330b386a4a1d
Souta112 <gameshhe75@gmail.com>
2021-07-29 12:32:31 +0000

    修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f532da4bc19e59c48c3dd0e46d4e98e250c74d14d44fc7f3ab32ae4a311e3f84415ed97738dad1968bbb8f0c14b39d0d709bdc61e638e8811c9f91785a39f33b'
